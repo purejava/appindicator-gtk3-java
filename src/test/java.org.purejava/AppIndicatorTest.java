5b28085631e474e85b0d88a7ef2efa0115653120
Ralph Plawetzki <ralph@purejava.org>
2023-04-01 18:30:50 +0200

    Add JUnit test for loading the native library

---
+++ src/test/java.org.purejava/AppIndicatorTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppIndicatorTest {
+    private static final Logger LOG = LoggerFactory.getLogger(AppIndicatorTest.class);
+    @Test
+    @Order(1)
+    @DisplayName("Testing availability of native lib")
+    @EnabledOnOs(OS.LINUX)
+    public void shouldHaveNoErrors() {
+        try {
+            System.loadLibrary("appindicator3");
+        } catch (UnsatisfiedLinkError e) {
+            LOG.error("Native code library failed to load.\n", e);
+        }
+    }
+}
