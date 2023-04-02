f15fe4c59f309aa717a6dbdb1633c7f056e7ac15
Ralph Plawetzki <ralph@purejava.org>
2023-04-02 19:14:06 +0200

    Provide method to set callback

--- src/main/java/org/purejava/linux/MemoryAllocator.java
+++ src/main/java/org/purejava/linux/MemoryAllocator.java
@@ -1,9 +1,14 @@
 package org.purejava.linux;
 
 import java.lang.foreign.MemorySegment;
+import java.lang.foreign.MemorySession;
 
 public class MemoryAllocator {
     public static MemorySegment ALLOCATE_FOR(String str) {
         return RuntimeHelper.CONSTANT_ALLOCATOR.allocateUtf8String(str);
+    }
+
+    public static MemorySegment ALLOCATE_CALLBACK_FOR(GCallback cb, MemorySession session) {
+        return GCallback.allocate(cb, session);
     }
 }
