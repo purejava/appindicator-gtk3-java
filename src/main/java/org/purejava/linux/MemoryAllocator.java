49c809c2fd15f63cc05a18cf7f60971ecf9bc658
Ralph Plawetzki <ralph@purejava.org>
2023-04-27 17:18:56 +0200

    MemorySession has been replaced by Arena and SegmentScope

--- src/main/java/org/purejava/linux/MemoryAllocator.java
+++ src/main/java/org/purejava/linux/MemoryAllocator.java
@@ -1,15 +1,15 @@
 package org.purejava.linux;
 
 import java.lang.foreign.MemorySegment;
-import java.lang.foreign.MemorySession;
+import java.lang.foreign.SegmentScope;
 
 public class MemoryAllocator {
     public static MemorySegment ALLOCATE_FOR(String str) {
         return RuntimeHelper.CONSTANT_ALLOCATOR.allocateUtf8String(str);
     }
 
-    public static MemorySegment ALLOCATE_CALLBACK_FOR(GCallback cb, MemorySession session) {
-        return GCallback.allocate(cb, session);
+    public static MemorySegment ALLOCATE_CALLBACK_FOR(GCallback cb, SegmentScope scope) {
+        return GCallback.allocate(cb, scope);
     }
 
     public static boolean isLoadedNativeLib() {
