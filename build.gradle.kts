ace9eb9e0108a071a03899dd2ea537a2d5c5f931
Ralph Plawetzki <ralph@purejava.org>
2025-05-24 15:09:30 +0200

    Fix TestSuiteExecutionException: Could not complete execution for Gradle Test Executor 3

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,7 @@ dependencies {
     testImplementation(libs.org.junit.jupiter.junit.jupiter.api)
     testImplementation(libs.org.junit.jupiter.junit.jupiter.engine)
     testImplementation(libs.org.junit.jupiter.junit.jupiter)
+    testRuntimeOnly(libs.org.junit.platform.junit.platform.launcher)
     testImplementation(libs.org.slf4j.slf4j.simple)
 }
 
