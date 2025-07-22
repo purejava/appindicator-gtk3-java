0abfea6d2a9feb05ac9aa7f4002a41725896c7eb
Ralph Plawetzki <ralph@purejava.org>
2025-07-22 14:04:12 +0200

    Lower log severity for /etc/ld.so.conf.d/ not found

--- src/main/java/org/purejava/appindicator/AppIndicator.java
+++ src/main/java/org/purejava/appindicator/AppIndicator.java
@@ -40,7 +40,7 @@ public final class AppIndicator {
                 }
             });
         } catch (IOException e) {
-            LOG.error("Directory '{}' does not exist", LD_CONFIG);
+            LOG.warn("Directory '{}' does not exist", LD_CONFIG);
         }
 
         allPath.add("/usr/lib"); // for systems, that don't implement multiarch
